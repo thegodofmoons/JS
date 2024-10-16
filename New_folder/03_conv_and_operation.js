8af5e0057f779c89aaddfa198c4e4aaae847ccf0
thegodofmoons <thegodofmoons@gmail.com>
2024-10-16 20:29:17 +0000

    conversion

---
+++ New_folder/03_conv_and_operation.js
@@ -0,0 +1,28 @@
+// let score = "hit"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; // false => 0
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// results:-
+// 1 => true
+// "" => false
+// "hitesh" => ture
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
