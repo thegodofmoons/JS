535e4e0de1442c36d26da9a63da8f8b77de36db6
thegodofmoons <thegodofmoons@gmail.com>
2024-10-17 18:53:13 +0000

    conversion

--- New_folder/03_conv_and_operation.js
+++ New_folder/03_conv_and_operation.js
@@ -21,8 +21,41 @@
 // "" => false
 // "hitesh" => ture
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/* conversion */
+
+// let value = 1
+// let negValue = -value
+
+// console.log(negValue);
+// we can add new value and manipulate the given result
+
+// console.log("1" + 1 + 1);
+// we got 11 as output cause 1st vaue is a str so others will also treat same.
+
+// console.log(1 + 1 + "1");
+// output is 21 for same reason 1st and 2nd value num and 3rd is str.
+
+// console.log(true);
+// gonna treat as boolean
+
+// console.log(+true);
+// ouput will be 1
+
+// console.log(+"");
+// output wll be 0
+
+// let gameValue = 20
+// gameValue++;
+// console.log(gameValue);
+
+// value => 21 
+// go and study more about prefix and postfix
+
+
+
