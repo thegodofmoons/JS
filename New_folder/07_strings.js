3891c70a1af61a0526bb75927a7d0254d6fe51e2
SOM DEV <thegodofmoons@gmail.com>
2024-10-19 20:37:25 +0000

    strings

---
+++ New_folder/07_strings.js
@@ -0,0 +1,26 @@
+const name = "Som"
+const repoCount = 1
+
+// console.log(name + repoCount + "Value")
+
+/*String interpulation use this mordernday way*/
+console.log(`Hello my name is ${name} and my repoCount ${repoCount}`);
+
+const gameName = new String(`sorry-saktiman`)
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+const url = "https://google.com"
+
+console.log(url.replace(`%20`, '-'));
+
+console.log(url.includes(`sundar`));
+
+console.log(gameName.split('-'));
+
+// go and learn new methods 
+
+
