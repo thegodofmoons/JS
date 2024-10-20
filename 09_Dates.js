718a5151ea7522a6e6ebe11434b963357c867796
SOM DEV <thegodofmoons@gmail.com>
2024-10-20 07:53:41 +0000

    Dates

---
+++ 09_Dates.js
@@ -0,0 +1,34 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date("01-14-2023");
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleDateString('default', {
+    weekday: "long",
+})
+
+
+
+
+
+
+
