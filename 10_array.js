d312f386dd37439832d00fc741fc2c2d2866dc28
SOM DEV <thegodofmoons@gmail.com>
2024-10-20 08:35:26 +0000

    array

---
+++ 10_array.js
@@ -0,0 +1,45 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 5]
+const myHeros =["shaktiman", "krish"]
+
+const myArray1 = new Array(1, 2, 3, 4)
+// console.log(myArray[1]);
+
+// Array methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9)
+
+console.log(myArray.includes(9));
+console.log(myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
